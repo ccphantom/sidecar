ad300d7e04e6500014bd4d052c85431d27f4dcc1
Jack Chen <byjackchen@gmail.com>
2021-11-24 01:08:55 -0600

    Build the benefit calculation function for inital testing

--- srv/lib/calculate-benefit-service.js
+++ srv/lib/calculate-benefit-service.js
@@ -7,45 +7,140 @@ class CalculateBenefitService extends cds.ApplicationService {
         await super.init()
     }
     async calculateBenefit(req) {
+
         let { benefitBase, benefitOverride, benefitCumulation, customerInfo } = req.data;
-        if ( benefitBase.length == 0 ) return;
-        benefitBase.sort((a, b) => (a.workdate.localeCompare(b.workdate) || a.earnCode.localeCompare(b.earnCode) || a.baseCode.localeCompare(b.baseCode) || a.globalUnionCode.localeCompare(b.globalUnionCode)));
+        // 1. Initialize Response
+        const unionBenefit = {
+            customerID: null, workdate: null, benefitCode: null,
+            hours: null, rate: null, amount: null,
+            globalUnionCode: null, globalClassCode: null, globalCraftCode: null,
+            sapALZNR: null, sapC1ZNR: null, sapABART: null,
+            sapAPZNR: null, sapUNPTR: null, sapTRFGR: null,
+            sapTRFST: null, sapPOSNR: null,
+        };
+        const unionBenefitArray = [];
+        const returnData = { message: null, unionBenefit: unionBenefitArray }
 
+        // 2. Check if there is valid data feed
+        if (benefitBase.length == 0) {
+            returnData.message = 'No data feed';
+            return req.reply(returnData)
+        };
+        // 3. Read from Database
         const db = await cds.connect.to('db');
-        const { Unions, Crafts, Classes, UnionRates } = db.entities('com.reachnett.union');
-        const { unions } = await db.read(Unions);
+        const { Unions, Crafts, Classes, UnionRates, UnionFringes } = db.entities('com.reachnett.union');
+        const criteriaGlobalUnionCode = [...new Set(benefitBase.map(element => {
+            return element.globalUnionCode
+        }))];
 
-        let unionBenefitArray = [];
-        benefitBase.forEach(element => {
-            let amount = element.hours * element.rate;
-            let unionBenefit = {
-                customerID      : "",
-                workdate        : element.workdate,
-                benefitCode     : "",
-                hours           : element.hours,
-                rate            : element.rate,
-                amount          : amount,
-                globalUnionCode : element.globalUnionCode,
-                globalClassCode : "",
-                globalCraftCode : "",
-                sapALZNR        : "",
-                sapC1ZNR        : "",
-                sapABART        : "",
-                sapAPZNR        : "",
-                sapUNPTR        : "",
-                sapTRFGR        : "",
-                sapTRFST        : "",
-                sapPOSNR        : "",
-            };
-            unionBenefitArray.push(unionBenefit)
+        const unionFringes = await db.read(UnionFringes).where({
+            customerID: customerInfo.customerID,
+            and: { unionCode: { in: criteriaGlobalUnionCode } }
         });
 
-        let returnData = {
-            message : 'dummy return message',
-            unionBenefit : unionBenefitArray
-        }
 
-        return req.reply( returnData )
+        // 4. Start Processing Benefit Base Line Items
+        benefitBase.sort((a, b) => (
+            a.workdate.localeCompare(b.workdate) ||
+            a.earnCode.localeCompare(b.earnCode) ||
+            a.baseCode.localeCompare(b.baseCode) ||
+            a.globalUnionCode.localeCompare(b.globalUnionCode)
+        ));
+
+        benefitBase.forEach(elmtEarning => {
+            // 4.1. Gather all associated benefits for this line item setup
+            let candidateBenefits = JSON.parse(JSON.stringify(unionFringes));                            //deep copy
+            let candidatePersonalBenefits = JSON.parse(JSON.stringify(benefitOverride));
+            candidateBenefits = candidateBenefits.filter(elmt => {                                       // knock out unrelative
+                return ((elmt.unionCode == elmtEarning.globalUnionCode || elmt.unionCode == '*') &&
+                    (elmt.unionClass == elmtEarning.globalUnionClass || elmt.unionClass == '*') &&
+                    (elmt.unionCraft == elmtEarning.globalUnionCraft || elmt.unionCraft == '*') &&
+                    (elmt.projectID == elmtEarning.projectID || elmt.projectID == '*') &&
+                    (elmt.validFrom <= elmtEarning.workdate && elmt.validTo >= elmtEarning.workdate)
+                );
+            })
+
+            candidatePersonalBenefits = candidatePersonalBenefits.filter(elmt => {
+                return ((elmt.unionCode == elmtEarning.globalUnionCode || elmt.unionCode == '*') &&
+                    (elmt.unionClass == elmtEarning.globalUnionClass || elmt.unionClass == '*') &&
+                    (elmt.unionCraft == elmtEarning.globalUnionCraft || elmt.unionCraft == '*') &&
+                    (elmt.projectID == elmtEarning.projectID || elmt.projectID == '*') &&
+                    (elmt.beginDate <= elmtEarning.workdate && elmt.endDate >= elmtEarning.workdate)
+                );
+            })
+
+            let personalBenefitCodes = [...new Set(candidatePersonalBenefits.map(e => e.benefitCode))];  // merge generic into personal benefits if not duplicated
+            candidateBenefits.forEach(elmt => {
+                if (!personalBenefitCodes.includes(elmt.unionFringe)) {
+                    let helpArray = {
+                        customerID: elmt.customerID,
+                        unionInfoPointer: elmt.unionInfoPointer,
+                        benefitCode: elmt.unionFringe,
+                        projectID: elmt.projectID,
+                        endDate: elmt.validTo,
+                        beginDate: elmt.validFrom,
+                        benefitDescrition: elmt.fringeDescription,
+                        benefitRate: elmt.fringeRate,
+                        calcMethod: elmt.calculationMethod,
+                        baseBucket: elmt.baseCode,
+                        paymentModel: elmt.paymentModel,
+                        unionCode: elmt.unionCode,
+                        unionCraft: elmt.unionCraft,
+                        unionClass: elmt.unionClass
+                    }
+                    candidatePersonalBenefits.push(helpArray);
+                }
+            })
+
+            let tempBenefits = [];                                                                        // prioritize
+            tempBenefits = candidatePersonalBenefits;  // Need to add Prioritize Logic
+            candidatePersonalBenefits = tempBenefits;
+
+            //4.2 Calculate
+            candidatePersonalBenefits.forEach(elmtBenefit => {
+                unionBenefit.customerID = elmtBenefit.customerID;
+                unionBenefit.workdate = elmtEarning.workdate;
+                unionBenefit.benefitCode = elmtBenefit.benefitCode;
+                unionBenefit.globalUnionCode = elmtBenefit.unionCode;
+                unionBenefit.globalUnionCraft = elmtBenefit.unionCraft;
+                unionBenefit.globalUnionClass = elmtBenefit.unionClass;
+                unionBenefit.sapALZNR = elmtEarning.sapALZNR;
+                unionBenefit.sapC1ZNR = elmtEarning.sapC1ZNR;
+                unionBenefit.sapABART = elmtEarning.sapABART;
+                unionBenefit.sapAPZNR = elmtEarning.sapAPZNR;
+                unionBenefit.sapUNPTR = elmtEarning.sapUNPTR;
+                unionBenefit.sapTRFGR = elmtEarning.sapTRFGR;
+                unionBenefit.sapTRFST = elmtEarning.sapTRFST;
+                unionBenefit.sapPOSNR = elmtEarning.sapPOSNR;
+
+                switch (elmtBenefit.calcMethod) {
+                    case 'H': // hourly rate
+                        unionBenefit.rate = elmtBenefit.benefitRate;
+                        unionBenefit.hours = elmtEarning.hours;
+                        unionBenefit.amount = unionBenefit.hours * unionBenefit.rate;
+                        break;
+                    case 'F': // fixed amount
+                        unionBenefit.rate = elmtBenefit.benefitRate;
+                        unionBenefit.hours = elmtEarning.hours;
+                        unionBenefit.amount = elmtBenefit.benefitRate;
+                        break;
+                    case 'A': // percentage
+                        unionBenefit.rate = elmtEarning.rate;
+                        unionBenefit.hours = elmtEarning.hours;
+                        unionBenefit.amount = elmtEarning.amount * elmtBenefit.benefitRate / 100;
+                        break;
+                    default:
+                        break;
+                }
+
+                unionBenefitArray.push(unionBenefit);                                                      // append
+            })
+
+        });
+
+        // 5. Wrap up response
+        returnData.message = 'Benefit Calculated Successfully';
+        return req.reply(returnData)
     }
 }
 module.exports = CalculateBenefitService
